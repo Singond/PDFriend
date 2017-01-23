c7520565ff4992aabc9da8f15fa50a904bbc1810
Jan Singon Slaný <slanyj@centrum.cz>
2017-01-23 08:17:58 +0100

    Implement imposing Signature

--- src/cz/slanyj/pdfriend/book/Signature.java
+++ src/cz/slanyj/pdfriend/book/Signature.java
@@ -1,5 +1,12 @@
 package cz.slanyj.pdfriend.book;
 
+import java.io.IOException;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+
 /**
  * A signature of a document, made by folding one or more Sheets.
  * @author Sorondil
@@ -7,4 +14,37 @@ package cz.slanyj.pdfriend.book;
  */
 public class Signature {
 
+	/** The sheets comprising this Signature */
+	private final Set<Sheet> sheets;
+	
+	public Signature() {
+		this.sheets = new LinkedHashSet<>();
+	}
+	
+	public void add(Sheet sheet) {
+		sheets.add(sheet);
+	}
+	
+	/**
+	 * Renders the given sheet into the given document as two new pages
+	 * (recto first, verso second).
+	 * @param i
+	 * @param doc
+	 */
+	private void renderSheet(Sheet sheet, PDDocument doc) throws IOException {
+		PDPage front = sheet.renderFront(doc);
+		doc.addPage(front);
+		PDPage back = sheet.renderBack(doc);
+		doc.addPage(back);
+	}
+	
+	/**
+	 * Renders all Sheets in this signature into the given document,
+	 * each as two new pages (recto first, verso second).
+	 */
+	public void renderAllSheets(PDDocument doc) throws IOException {
+		for (Sheet s : sheets) {
+			renderSheet(s, doc);
+		}
+	}
 }
---
+++ src/cz/slanyj/pdfriend/test/ImposeSignature.java
@@ -0,0 +1,90 @@
+package cz.slanyj.pdfriend.test;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import cz.slanyj.pdfriend.SourcePage;
+import cz.slanyj.pdfriend.book.Leaf;
+import cz.slanyj.pdfriend.book.Leaf.FlipDirection;
+import cz.slanyj.pdfriend.book.Leaf.Orientation;
+import cz.slanyj.pdfriend.book.Sheet;
+import cz.slanyj.pdfriend.book.Signature;
+
+/**
+ * A sample signature of two sheets.
+ * @author Sorondil
+ *
+ */
+public class ImposeSignature {
+
+	public static void main(String[] args) {
+		Leaf leaf = new Leaf(792, 612);
+		leaf.setXPosition(306);
+		leaf.setYPosition(396);
+		leaf.setRotation(Math.PI/2);
+		leaf.setOrientation(Orientation.RECTO_UP);
+		leaf.setFlipDirection(FlipDirection.AROUND_Y);
+		
+		Leaf leaf2 = new Leaf(792, 612);
+		leaf2.setXPosition(918);
+		leaf2.setYPosition(396);
+		leaf2.setRotation(Math.PI/2);
+		leaf2.setOrientation(Orientation.VERSO_UP);
+		leaf2.setFlipDirection(FlipDirection.AROUND_Y);
+		
+		Sheet sheet = new Sheet(1224, 792);
+		sheet.addLeaf(leaf);
+		sheet.addLeaf(leaf2);
+		
+		Leaf leaf3 = new Leaf(792, 612);
+		leaf3.setXPosition(306);
+		leaf3.setYPosition(396);
+		leaf3.setRotation(Math.PI/2);
+		leaf3.setOrientation(Orientation.RECTO_UP);
+		leaf3.setFlipDirection(FlipDirection.AROUND_Y);
+		
+		Leaf leaf4 = new Leaf(792, 612);
+		leaf4.setXPosition(918);
+		leaf4.setYPosition(396);
+		leaf4.setRotation(Math.PI/2);
+		leaf4.setOrientation(Orientation.VERSO_UP);
+		leaf4.setFlipDirection(FlipDirection.AROUND_Y);
+		
+		Sheet sheet2 = new Sheet(1224, 792);
+		sheet2.addLeaf(leaf3);
+		sheet2.addLeaf(leaf4);
+		
+		Signature signature = new Signature();
+		signature.add(sheet);
+		signature.add(sheet2);
+		
+		try {
+			// Get content
+			PDDocument source = PDDocument.load(new File("test/manual.pdf"));
+			SourcePage one = new SourcePage(source, 0);
+			SourcePage two = new SourcePage(source, 1);
+			SourcePage three = new SourcePage(source, 2);
+			SourcePage four = new SourcePage(source, 3);
+			SourcePage five = new SourcePage(source, 4);
+			SourcePage six = new SourcePage(source, 5);
+			SourcePage seven = new SourcePage(source, 6);
+			SourcePage eight = new SourcePage(source, 7);
+			leaf.setContent(one, two);
+			leaf2.setContent(three, four);
+			leaf3.setContent(five, six);
+			leaf4.setContent(seven, eight);
+			
+			PDDocument doc = new PDDocument();
+			signature.renderAllSheets(doc);
+			
+			// Save
+			doc.save(new File("test/imposed-signature.pdf"));
+			doc.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
