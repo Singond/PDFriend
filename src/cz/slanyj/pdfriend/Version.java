411b7b51b5a06fefc49306d0705bf4573d4d91ea
Jan Singon Slaný <slanyj@centrum.cz>
2017-03-05 20:08:15 +0100

    Clean imports

--- src/cz/slanyj/pdfriend/Version.java
+++ src/cz/slanyj/pdfriend/Version.java
@@ -1,10 +1,6 @@
 package cz.slanyj.pdfriend;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
 import java.util.ResourceBundle;
 
 public class Version {
