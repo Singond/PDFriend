580ebc6fdd4c9454edc069d8f33091950d1e0c4d
Jan Singon Slaný <slanyj@centrum.cz>
2017-04-25 01:50:38 +0200

    Add document renderer class

---
+++ src/cz/slanyj/pdfriend/document/Renderer.java
@@ -0,0 +1,29 @@
+package cz.slanyj.pdfriend.document;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+/**
+ * Converts the virtual document into a concrete document.
+ * The type of the output document depends on the Renderer subclass.
+ * @author Singon
+ *
+ */
+public abstract class Renderer {
+
+	/**
+	 * Renders the given virtual document into a real document.
+	 */
+	public abstract byte[] render(Document document);
+	
+	/**
+	 * Renders the virtual document into a real document and saves
+	 * it to the given file.
+	 * @throws IOException if an I/O error occurs writing to or creating the file.
+	 */
+	public void renderAndSave(Document document, File output) throws IOException {
+		byte[] data = render(document);
+		Files.write(output.toPath(), data);
+	}
+}
