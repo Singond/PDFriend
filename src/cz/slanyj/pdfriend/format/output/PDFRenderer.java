2ba318204dfd79101edcc6a9b0237a4c7831450b
Jan Slany <jan.slany@ais-servis.cz>
2017-04-26 00:14:33 +0200

    Add PDF Renderer

---
+++ src/cz/slanyj/pdfriend/format/output/PDFRenderer.java
@@ -0,0 +1,14 @@
+package cz.slanyj.pdfriend.format.output;
+
+import cz.slanyj.pdfriend.document.Document;
+import cz.slanyj.pdfriend.document.Renderer;
+
+public class PDFRenderer extends Renderer {
+
+	@Override
+	public byte[] render(Document document) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
