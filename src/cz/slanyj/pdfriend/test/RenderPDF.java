a7e0b8776c452886db6635f2b2d9afe12ba1090c
Jan Slany <jan.slany@ais-servis.cz>
2017-04-30 11:12:54 +0200

    Test PDF rendering on sample VirtualDocument

---
+++ src/cz/slanyj/pdfriend/test/RenderPDF.java
@@ -0,0 +1,50 @@
+package cz.slanyj.pdfriend.test;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+
+import cz.slanyj.pdfriend.document.RenderingException;
+import cz.slanyj.pdfriend.document.VirtualDocument;
+import cz.slanyj.pdfriend.document.VirtualPage;
+import cz.slanyj.pdfriend.format.content.PDFPage;
+import cz.slanyj.pdfriend.format.output.PDFRenderer;
+
+public class RenderPDF {
+
+	public static void main(String[] args) {
+
+		try {
+			/* Source */
+			PDDocument source = PDDocument.load(new File("test/lorem-letter.pdf"));
+			
+			/* Virtual Document */
+			VirtualDocument.Builder doc = new VirtualDocument.Builder();
+			VirtualPage.Builder pg1 = new VirtualPage.Builder();
+			pg1.setWidth(612);
+			pg1.setHeight(792);
+			pg1.addContent(new PDFPage(source, 0));
+			doc.addPage(pg1.build());
+			VirtualPage.Builder pg2 = new VirtualPage.Builder();
+			pg2.setWidth(612);
+			pg2.setHeight(792);
+			pg2.addContent(new PDFPage(source, 1));
+			doc.addPage(pg2.build());
+			
+			/* Output */
+			VirtualDocument document = doc.build();
+			PDDocument out = new PDFRenderer().render(document);
+			out.save(new File("test/rendered-pdf.pdf"));
+			out.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (RenderingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
