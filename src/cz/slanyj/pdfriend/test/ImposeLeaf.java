f1b9787df8cb0b8b4175a2bc028ca9e82d755728
Jan Slany <jan.slany@ais-servis.cz>
2017-05-07 11:46:20 +0200

    Test imposing Leaf

--- src/cz/slanyj/pdfriend/test/ImposeLeaf.java
+++ src/cz/slanyj/pdfriend/test/ImposeLeaf.java
@@ -2,6 +2,7 @@ package cz.slanyj.pdfriend.test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 import org.apache.pdfbox.multipdf.LayerUtility;
 import org.apache.pdfbox.pdmodel.PDDocument;
@@ -13,6 +14,11 @@ import org.apache.pdfbox.util.Matrix;
 import cz.slanyj.pdfriend.book.FlipDirection;
 import cz.slanyj.pdfriend.book.Leaf;
 import cz.slanyj.pdfriend.book.Leaf.Orientation;
+import cz.slanyj.pdfriend.document.RenderingException;
+import cz.slanyj.pdfriend.document.VirtualDocument;
+import cz.slanyj.pdfriend.document.VirtualPage;
+import cz.slanyj.pdfriend.format.content.PDFPage;
+import cz.slanyj.pdfriend.format.output.PDFRenderer;
 import cz.slanyj.pdfriend.impose.formats.PDFSourcePage;
 
 /**
@@ -30,39 +36,54 @@ public class ImposeLeaf {
 		leaf.setOrientation(Orientation.RECTO_UP);
 		leaf.setFlipDirection(FlipDirection.AROUND_Y);
 		
-		PDDocument doc = new PDDocument();
-		PDPage sheetFront = new PDPage();
-		PDPage sheetBack = new PDPage();
-		sheetFront.setMediaBox(new PDRectangle(1224, 792));
-		sheetBack.setMediaBox(new PDRectangle(1224, 792));
-		doc.addPage(sheetFront);
-		doc.addPage(sheetBack);
+//		PDDocument doc = new PDDocument();
+//		PDPage sheetFront = new PDPage();
+//		PDPage sheetBack = new PDPage();
+//		sheetFront.setMediaBox(new PDRectangle(1224, 792));
+//		sheetBack.setMediaBox(new PDRectangle(1224, 792));
+//		doc.addPage(sheetFront);
+//		doc.addPage(sheetBack);
 		
 		PDPageContentStream content;
 		
 		try {
 			// Get content
 			PDDocument source = PDDocument.load(new File("test/lorem-letter.pdf"));
-			PDFSourcePage one = new PDFSourcePage(source, 0);
-			PDFSourcePage two = new PDFSourcePage(source, 1);
-			leaf.setContent(one, two);
+			VirtualPage one = new VirtualPage(612, 792, Arrays.asList(new PDFPage(source, 0)));
+			VirtualPage two = new VirtualPage(612, 792, Arrays.asList(new PDFPage(source, 1)));
+//			PDFPage one = new PDFPage(source, 0);
+//			PDFPage two = new PDFPage(source, 1);
+			leaf.getFrontPage().setSource(one);
+			leaf.getBackPage().setSource(two);
 			
 			// Impose
-			content = new PDPageContentStream(doc, sheetFront, PDPageContentStream.AppendMode.APPEND, true);
-			LayerUtility lu = new LayerUtility(doc);
-			leaf.imposeFront(content, lu);
-			content.close();
-			content = new PDPageContentStream(doc, sheetBack, PDPageContentStream.AppendMode.APPEND, true);
-			float width = sheetFront.getMediaBox().getWidth();
-			content.transform(Matrix.getTranslateInstance(width, 0));
-			content.transform(Matrix.getScaleInstance(-1, 1));
-			leaf.imposeBack(content, lu);
-			content.close();
+			VirtualDocument.Builder doc = new VirtualDocument.Builder();
+			doc.addPage(one);
+			doc.addPage(two);
+			
+			// Output
+			VirtualDocument document = doc.build();
+			PDDocument out = new PDFRenderer().render(document);
+			out.save(new File("test/imposed-leaf.pdf"));
+			out.close();
+			
+//			content = new PDPageContentStream(doc, sheetFront, PDPageContentStream.AppendMode.APPEND, true);
+//			LayerUtility lu = new LayerUtility(doc);
+//			leaf.imposeFront(content, lu);
+//			content.close();
+//			content = new PDPageContentStream(doc, sheetBack, PDPageContentStream.AppendMode.APPEND, true);
+//			float width = sheetFront.getMediaBox().getWidth();
+//			content.transform(Matrix.getTranslateInstance(width, 0));
+//			content.transform(Matrix.getScaleInstance(-1, 1));
+//			leaf.imposeBack(content, lu);
+//			content.close();
 			
 			// Save
-			doc.save(new File("test/imposed-leaf.pdf"));
-			doc.close();
+//			doc.save(new File("test/imposed-leaf.pdf"));
+//			doc.close();
 		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (RenderingException e) {
 			e.printStackTrace();
 		}
 	}
