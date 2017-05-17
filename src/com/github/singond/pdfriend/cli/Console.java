438e9bad2ec878457d1fdcb93893ee164c9cdcea
Jan Singon Slany <slanyj@centrum.cz>
2017-05-18 01:00:00 +0200

    Split arg array per subcommands before parsing

--- src/com/github/singond/pdfriend/cli/Console.java
+++ src/com/github/singond/pdfriend/cli/Console.java
@@ -1,7 +1,9 @@
 package com.github.singond.pdfriend.cli;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.logging.log4j.Level;
@@ -27,31 +29,29 @@ public class Console {
 	private final GlobalOptions global = new GlobalOptions();
 	/** Logger */
 	private static ExtendedLogger logger = Log.logger(Console.class);
+	/** Pattern to split the argument array at. This must be one element. */
+	private static final String SUBCOMMAND_DELIMITER = "+";
 	
 	/** Subcommands (ie. modules) */
-	private final Map<String, SubCommand> subcommands = new HashMap<>();
-	{
-		subcommands.put("impose", new Impose());
-	}
+	private static class SubCommands extends HashMap<String, SubCommand> {{
+		put("impose", new Impose());
+	}}
 	
 	/**
 	 * Run PDFriend in command-line mode.
 	 * @param args the whole argument array passed into the program
 	 */
 	public void execute(String[] args) {
-		/* Parse the CLI arguments */
-		JCommander.Builder jcbuilder = JCommander.newBuilder()
-				.addObject(global)
-				.acceptUnknownOptions(false);
-		// Register subcommands with the parser
-		for (Map.Entry<String, SubCommand> cmd : subcommands.entrySet()) {
-			jcbuilder.addCommand(cmd.getKey(), cmd.getValue());
-		}
-		JCommander jcommander = jcbuilder.build();
-		jcommander.parse(args);
+		List<List<String>> splitArgs = splitArgs(Arrays.asList(args));
+		/** Parsed subcommands */
+		List<SubCommand> subcommands = new ArrayList<>(splitArgs.size());
+		
+		parseGlobalArguments(splitArgs.get(0), global);
+		parseSubCommands(splitArgs, subcommands);
+
 		// Set verbosity level as early as possible
 		setVerbosity(global.quiet(), global.verbose(), global.debug());
-		SubCommand subcommand = subcommands.get(jcommander.getParsedCommand());
+		SubCommand subcommand = subcommands.get(0);
 		subcommand.postParse();
 		
 		/* Run */
@@ -68,12 +68,79 @@ public class Console {
 		
 		// Display help and exit (--help)
 		if (global.help()) {
-			help(jcommander);
+//			help(jcommander);
 			System.exit(0);
 		}
 		
 		// End global-level option processing and run the subcommand
 		subcommand.execute();
+	}
+	
+	/**
+	 * Splits the given list of arguments into several sublists at the
+	 * elements matching {@code SUBCOMMAND_DELIMITER}.
+	 * @param args
+	 * @return
+	 */
+	private List<List<String>> splitArgs(List<String> args) {
+		List<List<String>> result = new ArrayList<>();
+		List<String> sublist = new ArrayList<>();
+		for (String elem : args) {
+			if (isDelimiter(elem)) {
+				result.add(sublist);
+				sublist = new ArrayList<>();
+			} else {
+				sublist.add(elem);
+			}
+		}
+		result.add(sublist);	// Add the last sublist
+		return result;
+	}
+	
+	/**
+	 * Checks whether the given String is to be considered
+	 * a subcommand delimiter.
+	 * @param element the String to be checked
+	 * @return true if it is a delimiter
+	 */
+	private boolean isDelimiter(String element) {
+		return SUBCOMMAND_DELIMITER.equals(element);
+	}
+	
+	/**
+	 * Parses the arguments list into the given global options object.
+	 * @param arguments a list of the command-line arguments up to
+	 *        (and including) the first subcommand
+	 * @param target the global options object to receive the parsed values
+	 */
+	private void parseGlobalArguments(List<String> arguments,
+	                                  GlobalOptions target) {
+		JCommander.Builder globalParserBldr = JCommander.newBuilder()
+				.addObject(target)
+				.acceptUnknownOptions(true);
+		JCommander globalParser = globalParserBldr.build();
+		globalParser.parse(arguments.toArray(new String[arguments.size()]));
+	}
+	
+	/**
+	 * Parses each of the split arguments into a new SubCommand and collects
+	 * the parsed subcommands into the given list.
+	 * @param splitArgs a 2D list of arguments, with each sublist standing
+	 *        for one subcommand and its arguments
+	 * @param subCmdList the list to put parsed subcommands into
+	 */
+	private void parseSubCommands(List<List<String>> splitArgs,
+	                              List<SubCommand> subCmdList) {
+		for (List<String> sublist : splitArgs) {
+			SubCommands subcmds = new SubCommands();
+			JCommander.Builder subCmdParserBldr = JCommander.newBuilder();
+			for (Map.Entry<String, SubCommand> cmd : subcmds.entrySet()) {
+				subCmdParserBldr.addCommand(cmd.getKey(), cmd.getValue());
+			}
+			JCommander subCmdParser = subCmdParserBldr.build();
+			subCmdParser.parse(sublist.toArray(new String[sublist.size()]));
+			subCmdList.add(subcmds.get(subCmdParser.getParsedCommand()));
+		}
 	}
 	
 	/** Prints version info. */
