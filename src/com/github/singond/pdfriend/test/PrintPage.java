6f0e1757e3674b76fc7d8770663deae72afb559d
Jan Singon Slany <slanyj@centrum.cz>
2017-06-18 12:00:42 +0200

    Testing direct rendering of MultiPage

--- src/com/github/singond/pdfriend/test/PrintPage.java
+++ src/com/github/singond/pdfriend/test/PrintPage.java
@@ -4,7 +4,8 @@ import java.io.File;
 import java.io.IOException;
 
 import com.github.singond.pdfriend.book.control.SequentialSourceProvider;
-import com.github.singond.pdfriend.book.model.Page;
+import com.github.singond.pdfriend.book.control.SourceProvider;
+import com.github.singond.pdfriend.book.model.GridPage;
 import com.github.singond.pdfriend.book.model.SinglePage;
 import com.github.singond.pdfriend.document.ImportException;
 import com.github.singond.pdfriend.document.RenderingException;
@@ -15,13 +16,20 @@ import com.github.singond.pdfriend.format.process.PDFRenderer;
 public class PrintPage {
 
 	public static void main(String[] args) throws ImportException, RenderingException, IOException {
-		Page page1 = new SinglePage(612, 792);
 		File srcFile = new File("test/lorem-letter.pdf");
 		File output = new File("test/printed-page.pdf");
 		VirtualDocument source = new PDFImporter(srcFile).importDocument();
-		new SequentialSourceProvider(source).setSourceTo(page1);
+
+		SinglePage page1 = new SinglePage(612, 792);
+		GridPage page2 = new GridPage(2, 2, 306, 396);
+		page2.fitPages();
+		
+		SourceProvider sp = new SequentialSourceProvider(source);
+		sp.setSourceTo(page1);
+		sp.setSourceTo(page2);
 		VirtualDocument.Builder doc = new VirtualDocument.Builder();
 		doc.addPage(page1.render());
+		doc.addPage(page2.render());
 		new PDFRenderer().renderAndSave(doc.build(), output);
 	}
 }
