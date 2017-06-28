b8c8656b7d19ae24d1f5e30baddc5eeacf44d175
Jan Singon Slany <slanyj@centrum.cz>
2017-06-28 18:38:46 +0200

    Add Overlay model

---
+++ src/com/github/singond/pdfriend/imposition/Overlay.java
@@ -0,0 +1,64 @@
+package com.github.singond.pdfriend.imposition;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.singond.pdfriend.ExtendedLogger;
+import com.github.singond.pdfriend.Log;
+import com.github.singond.pdfriend.book.control.SequentialSourceProvider;
+import com.github.singond.pdfriend.book.control.SourceProvider;
+import com.github.singond.pdfriend.book.model.LayeredPage;
+import com.github.singond.pdfriend.book.model.Page;
+import com.github.singond.pdfriend.document.VirtualDocument;
+
+/**
+ * A document consisting of layered pages. 
+ *
+ * @author Singon
+ */
+public class Overlay implements Imposable {
+
+	/** The pages in the document */
+	private final List<LayeredPage> pages;
+	
+	private static ExtendedLogger logger = Log.logger(Overlay.class);
+	
+	public Overlay(double width, double height, int pages, int layers) {
+		LayeredPage template = new LayeredPage(width, height, layers);
+		List<LayeredPage> pageList = new ArrayList<>(pages);
+		int pageNumber = 0;
+		while(pageList.size() < pages) {
+			LayeredPage page = new LayeredPage(template);
+			page.setNumber(++pageNumber);
+			pageList.add(page);
+		}
+		this.pages = pageList;
+	}
+	
+	public static Overlay from(List<VirtualDocument> docs) {
+		double[] dims = VirtualDocument.maxPageDimensions(docs);
+		int pages = VirtualDocument.getTotalLength(docs);
+		int layers = docs.size();
+		Overlay result = new Overlay(dims[0], dims[1], pages, layers);
+		
+		List<SourceProvider> sps = new ArrayList<>(layers);
+		for (VirtualDocument doc : docs) {
+			sps.add(new SequentialSourceProvider(doc));
+		}
+		// TODO Fill pagelets with source
+//		for (Page p : pages) {
+//			for (Pagelet)
+//		}
+		return result;
+	}
+	
+	@Override
+	public VirtualDocument getDocument() {
+		VirtualDocument.Builder doc = new VirtualDocument.Builder();
+		for (Page page : pages) {
+			doc.addPage(page.render());
+		}
+		return doc.build();
+	}
+
+}
