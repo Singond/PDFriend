46dc68382515dd5262236c137e10174fcf151ed5
Jan Singon Slany <slanyj@centrum.cz>
2017-07-28 22:02:18 +0200

    Adapt OutputFile

--- src/com/github/singond/pdfriend/cli/parsing/OutputFile.java
+++ src/com/github/singond/pdfriend/cli/parsing/OutputFile.java
@@ -1,10 +1,11 @@
 package com.github.singond.pdfriend.cli.parsing;
 
 import java.io.File;
-
 import com.beust.jcommander.Parameter;
 import com.github.singond.pdfriend.ExtendedLogger;
 import com.github.singond.pdfriend.Log;
+import com.github.singond.pdfriend.io.Output;
+import com.github.singond.pdfriend.io.OutputFactory;
 
 /**
  * The command-line argument for the output file.
@@ -20,6 +21,13 @@ public class OutputFile {
 	
 	public void postParse() {
 		logger.verbose("The output file:" + outputFile.getAbsolutePath());
+	}
+	
+	/**
+	 * Returns the output file wrapped in one Output object.
+	 */
+	public Output getOutput() {
+		return OutputFactory.of(outputFile.toPath());
 	}
 
 	/** Gets the output file */
