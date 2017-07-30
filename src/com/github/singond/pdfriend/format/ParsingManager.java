c80818627ca689fc96d9fe1bc0b8f0b3e431826f
Jan Singon Slany <slanyj@centrum.cz>
2017-07-30 12:57:38 +0200

    Clean line

--- src/com/github/singond/pdfriend/format/ParsingManager.java
+++ src/com/github/singond/pdfriend/format/ParsingManager.java
@@ -9,7 +9,6 @@ import com.github.singond.pdfriend.format.process.PDFParser;
 import com.github.singond.pdfriend.io.Input;
 import com.github.singond.pdfriend.io.InputElement;
 import com.github.singond.pdfriend.io.InputException;
-//import com.github.singond.pdfriend.io.InputVisitor;
 
 /**
  * Imports input data into virtual documents to be processed
