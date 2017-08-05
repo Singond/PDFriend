40cf230bd3ad3327dd35c77c524ce30d6f0adf94
Jan Singon Slany <slanyj@centrum.cz>
2017-08-05 12:08:05 +0200

    Move rectangle parsing into common parsing class

--- src/com/github/singond/pdfriend/cli/parsing/DimensionsParser.java
+++ src/com/github/singond/pdfriend/cli/parsing/DimensionsParser.java
@@ -7,6 +7,7 @@ import java.util.regex.Pattern;
 
 import com.github.singond.pdfriend.ExtendedLogger;
 import com.github.singond.pdfriend.Log;
+import com.github.singond.pdfriend.geometry.Dimensions;
 import com.github.singond.pdfriend.geometry.Length;
 import com.github.singond.pdfriend.geometry.LengthUnit;
 import com.github.singond.pdfriend.geometry.LengthUnits;
@@ -21,6 +22,8 @@ class DimensionsParser {
 	 * beginning of a string
 	 */
 	private static final Pattern NUMERIC_START = Pattern.compile("^\\d*(\\.\\d*)?");
+	/** Two length dimensions separated by 'x' */
+	private static final Pattern TWO_DIMENSIONS = Pattern.compile("\\d*(\\.\\d*)?.*x\\d*(\\.\\d*)?.*");
 	/**
 	 * A lookup of paper formats
 	 */
@@ -55,6 +58,7 @@ class DimensionsParser {
 
 	/**
 	 * Converts a string to a length object.
+	 * 
 	 * This variant allows to specify a default length unit, which will be
 	 * used to interpret the string if no other unit is found therein. 
 	 * @param arg the string to be parsed
@@ -99,7 +103,8 @@ class DimensionsParser {
 	}
 	
 	/**
-	 * Converts a string to a length object. 
+	 * Converts a string to a length object.
+	 * 
 	 * If no unit is given in {@code arg}, this method will return
 	 * an {@code Unparsable} object.
 	 * @param arg the string to be parsed
@@ -112,6 +117,7 @@ class DimensionsParser {
 	
 	/**
 	 * Converts a string to a paper format object.
+	 * 
 	 * @param arg the string to be parsed
 	 * @return a {@code Parsed} object wrapping the parsed result, or
 	 *         an {@code Unparsable} instance if the string cannot be parsed
@@ -121,5 +127,94 @@ class DimensionsParser {
 			return new Parsed<>(formats.get(arg));
 		else
 			return new Unparsable<>("Unknown format name: " + arg);
+	}
+	
+	/**
+	 * Converts a string to a rectangle size.
+	 * 
+	 * This method interprets the string as WIDTHxHEIGHT.
+	 * It is assumes that if only one unit is given, it is after the
+	 * second dimension. If it is given, it is used to parse the first
+	 * number as well. This enables shorhand notation like 20x30cm
+	 * to actually mean "20 cm (width) by 30 cm (height)".
+	 * 
+	 * @param arg the string to be parsed
+	 * @param dfltUnit the default length unit to be used if none is given
+	 * @return a {@code Parsed} object wrapping the parsed result, or
+	 *         an {@code Unparsable} instance if the string cannot be parsed.
+	 *         In the case of successful parsing, the returned Dimensions
+	 *         object's width and height correspond to the WIDTH and HEIGHT
+	 *         as interpreted here, respectively.
+	 */
+	ParsingResult<Dimensions> parseTwoDimensions(String arg, LengthUnit dfltUnit) {
+		String[] parts = arg.split("x", 2);
+		if (parts.length != 2) {
+			return new Unparsable<>("The string cannot be split into two parts at 'x'");
+		}
+		
+		ParsingResult<Dimensions> result =
+				new Unparsable<>("Unknown format definition: " + arg);
+		/*
+		 * Lets assume that if only one unit is given, it is after the
+		 * second dimension. If it is given, use it to parse the first
+		 * number as well. This enables shorhand notation like 20x30cm
+		 * to actually mean "20 cm by 30 cm". (See method docs.)
+		 */
+		ParsingResult<Length> len2 = parseLength(parts[1]);
+		ParsingResult<Length> len1;
+		if (len2.parsedSuccessfully()) {
+			// Use the unit from the 2nd part as the default for the 1st
+			len1 = parseLength(parts[0], len2.getResult().unit());
+			if (len1.parsedSuccessfully()) {
+				Length length1 = len1.getResult();
+				Length length2 = len2.getResult();
+				Dimensions dims = new Dimensions(length1, length2);
+				logger.debug("parse_rectangle_success", dims);
+				result = new Parsed<>(dims);
+			}
+		}
+		return result;
+	}
+	
+	/**
+	 * <p>
+	 * If the size is given as a standard format designation, like "A4",
+	 * it is assumed to be in the default orientation given in argument.
+	 * The task of providing the correct page rotation is left to the
+	 * responsibility of the client code.
+	 * </p>
+	 * <p>
+	 * If the size is given as two dimensions in the form NxM, then N is
+	 * interpreted as width and M as height. A length unit can be specified
+	 * for both N and M independently, for example {@code 20cmx0.3m} is
+	 * translated as 200 mm (width) by 300 mm (height). If the unit for N
+	 * is omitted, it is assumed to be the same as the unit given with M.
+	 * If both units are omitted, the default unit given in argument is used.
+	 * </p>
+	 * @param arg the string to be parsed
+	 * @param dfltUnit the default length unit to be used if none is given
+	 * @param dfltOrientation default orientation (PORTRAIT or LANDSCAPE)
+	 *        to be used if it cannot be determined from the string
+	 * @return a {@code Parsed} object wrapping the parsed result, or
+	 *         an {@code Unparsable} instance if the string cannot be parsed
+	 */
+	ParsingResult<Dimensions> parseRectangleSize(String arg, LengthUnit dfltUnit,
+	                                             PaperFormat.Orientation dfltOrientation) {
+		logger.debug("parse_rectangle", arg);
+		
+		// Parse standard formats like A-series or US Letter
+		ParsingResult<PaperFormat> format = parsePaperFormat(arg);
+		if (format.parsedSuccessfully())
+			return new Parsed<>(format.getResult().dimensions(PaperFormat.Orientation.PORTRAIT));
+		
+		// Parse the string as two dimensions (width x height)
+		if (TWO_DIMENSIONS.matcher(arg).matches()) {
+			ParsingResult<Dimensions> dims = parseTwoDimensions(arg, dfltUnit);
+			if (dims.parsedSuccessfully())
+				return dims;
+		}
+		
+		// Don't know how to parse this
+		return new Unparsable<>("Unknown format definition: " + arg);
 	}
 }
