52aed4881f843a0828530f4ee44f6a68431e6318
Jan Singon Slany <slanyj@centrum.cz>
2017-09-08 20:20:27 +0200

    Parse resizing and alignment

--- src/com/github/singond/pdfriend/cli/parsing/PageOptions.java
+++ src/com/github/singond/pdfriend/cli/parsing/PageOptions.java
@@ -2,10 +2,14 @@ package com.github.singond.pdfriend.cli.parsing;
 
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
+import com.github.singond.pdfriend.geometry.Dimensions;
 import com.github.singond.pdfriend.imposition.Preprocessor;
 
 @Parameters(resourceBundle = "Help")
 public class PageOptions implements ParameterDelegate {
+	
+	private static final TwoNumbers DEFAULT_ALIGNMENT = new TwoNumbers(0,0);
+	private static final Dimensions AUTO = Dimensions.dummy();
 
 	@Parameter(names={"-s", "--scale"},
 	           description="The scale of the content",
@@ -21,7 +25,13 @@ public class PageOptions implements ParameterDelegate {
 	           description="Page size behaviour after scaling and rotating",
 	           descriptionKey="param-resizing")
 	private ResizingBehaviour resize = ResizingBehaviour.NONE;
-
+	
+	@Parameter(names={"--align", "--alignment"},
+	           description="Alignment of the scaled, rotated and resized page",
+	           descriptionKey="param-alignment",
+	           converter=TwoNumbers.Converter.class)
+	private TwoNumbers align = DEFAULT_ALIGNMENT;
+	
 	@Override
 	public void postParse() throws ArgumentParsingException {
 		// TODO Auto-generated method stub
@@ -32,11 +42,13 @@ public class PageOptions implements ParameterDelegate {
 		Preprocessor.Settings settings = new Preprocessor.Settings();
 		settings.setScale(scale);
 		settings.setRotation(rotation);
-		
+		settings.setResizing(resize.value);
+		settings.setHorizontalAndVerticalAlignment
+				(align.getFirst(), align.getSecond());
 		return settings;
 	}
 	
-	private static enum ResizingBehaviour {
+	public static enum ResizingBehaviour {
 		NONE(Preprocessor.Resizing.NONE),
 		FIT(Preprocessor.Resizing.FIT),
 		FILL(Preprocessor.Resizing.FILL);
---
+++ src/com/github/singond/pdfriend/cli/parsing/TwoNumbers.java
@@ -0,0 +1,43 @@
+package com.github.singond.pdfriend.cli.parsing;
+
+import java.util.regex.Pattern;
+
+import com.beust.jcommander.IStringConverter;
+import com.beust.jcommander.ParameterException;
+
+public class TwoNumbers {
+	private final double first;
+	private final double second;
+	
+    public TwoNumbers(double first, double second) {
+		this.first = first;
+		this.second = second;
+	}
+
+	public double getFirst() {
+		return first;
+	}
+
+	public double getSecond() {
+		return second;
+	}
+
+	public static class Converter implements IStringConverter<TwoNumbers> {
+    	private static final Pattern TWO_NUMBERS = Pattern.compile("\\d*,\\d*");
+    	
+    	@Override
+    	public TwoNumbers convert(String arg) {
+    		if (!TWO_NUMBERS.matcher(arg).matches()) {
+    			throw new ParameterException("Wrong format of number pair: "+arg);
+    		}
+    		String[] dims = arg.split(",", 2);
+    		try {
+    			double first = Double.parseDouble(dims[0]);
+    			double second = Double.parseDouble(dims[1]);
+    			return new TwoNumbers(first, second);
+    		} catch (NumberFormatException e) {
+    			throw new ParameterException("Wrong number format", e);
+    		}
+    	}
+    }
+}
