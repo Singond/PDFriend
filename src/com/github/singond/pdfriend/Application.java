395f48f39b4bf22a235c502fbe392e73ca81e289
Jan Singon Slany <slanyj@centrum.cz>
2017-09-09 11:36:00 +0200

    Execute application in a container object

--- src/com/github/singond/pdfriend/Application.java
+++ src/com/github/singond/pdfriend/Application.java
@@ -3,9 +3,7 @@ package com.github.singond.pdfriend;
 import com.github.singond.pdfriend.cli.Console;
 
 /**
- * The main class; this executes PDFriend.
- * This is the branching point for the application, ie. when a subcommand
- * is given right after {@code pdfriend} command, it is executed.
+ * The main class; this represents a single instance of PDFriend.
  * 
  * <p>Exit codes:
  * <ul>
@@ -22,12 +20,10 @@ public class Application {
 	private static ExtendedLogger logger = Log.logger("Main");
 
 	/**
-	 * Runs PDFriend.
+	 * Runs this instance of PDFriend.
 	 */
-	public static void main(String[] args) {
+	private void execute(String[] args) {
 		long start = System.currentTimeMillis();
-		//logger.debug("The working directory is {}", Util.getWorkingDir());
-		//logger.debug("The application directory is {}", Util.getApplicationDir());
 		
 		if (args.length <= 0) {
 			// GUI (to be added later). For now, just print version.
@@ -38,5 +34,12 @@ public class Application {
 			long end = System.currentTimeMillis();
 			logger.info("total_time", end-start);
 		}
+	}
+	
+	/**
+	 * Runs PDFriend.
+	 */
+	public static void main(String[] args) {
+		new Application().execute(args);
 	}
 }
