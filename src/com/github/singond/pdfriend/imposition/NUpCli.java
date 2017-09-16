71ea9c0a59f4731d0a06b6289c459852eca8124c
Jan Singon Slany <slanyj@centrum.cz>
2017-09-17 00:12:56 +0200

    Add auto option for n-up grid dimensions

--- src/com/github/singond/pdfriend/imposition/NUpCli.java
+++ src/com/github/singond/pdfriend/imposition/NUpCli.java
@@ -1,5 +1,6 @@
 package com.github.singond.pdfriend.imposition;
 
+import com.beust.jcommander.IStringConverter;
 import com.beust.jcommander.Parameter;
 import com.github.singond.pdfriend.cli.ParameterConsistencyException;
 import com.github.singond.pdfriend.cli.IntegerDimensionsConverter;
@@ -13,9 +14,16 @@ import com.github.singond.pdfriend.geometry.IntegerDimensions;
  */
 class NUpCli implements ImposableCli<NUp> {
 
+	/**
+	 * Use null to represent AUTO. Use this internally and do not expose
+	 * this smelly piece.
+	 * TODO It stinks anyway. Any nice way to avoid the null?
+	 * Maybe just store the string and parse it manually when constructing
+	 * the output object in {@link #getImposable()}.
+	 */
 	@Parameter(names={"--n-up", "--nup"},
 			description="Several pages arranged into a grid on a larget sheet",
-			converter=IntegerDimensionsConverter.class)
+			converter=NUpConverter.class)
 	private IntegerDimensions nup = null;
 
 	@Override
@@ -31,8 +39,23 @@ class NUpCli implements ImposableCli<NUp> {
 	@Override
 	public NUp getImposable() {
 		NUp task = new NUp();
-		task.setRows(nup.getFirstDimension());
-		task.setCols(nup.getSecondDimension());
+		if (nup != null) {
+			task.setRows(nup.getFirstDimension());
+			task.setCols(nup.getSecondDimension());
+		} else {
+			// TODO Pass to task!
+		}
 		return task;
+	}
+	
+	private static class NUpConverter implements IStringConverter<IntegerDimensions> {
+		@Override
+		public IntegerDimensions convert(String arg) {
+			if ("auto".equalsIgnoreCase(arg)) {
+				return null;
+			} else {
+				return new IntegerDimensionsConverter().convert(arg);
+			}
+		}
 	}
 }
