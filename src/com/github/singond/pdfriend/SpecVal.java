ffc8b5376baa8a0466af64aa718a358fec898f01
Jan Singon Slany <slanyj@centrum.cz>
2017-11-05 22:17:22 +0100

    Generate equals() and hashCode() for SpecVal

--- src/com/github/singond/pdfriend/SpecVal.java
+++ src/com/github/singond/pdfriend/SpecVal.java
@@ -83,4 +83,42 @@ public abstract class SpecVal<T extends Enum<T>, V> {
 			return special.toString();
 		}
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((special == null) ? 0 : special.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof SpecVal)) {
+			return false;
+		}
+		SpecVal<?, ?> other = (SpecVal<?, ?>) obj;
+		if (special == null) {
+			if (other.special != null) {
+				return false;
+			}
+		} else if (!special.equals(other.special)) {
+			return false;
+		}
+		if (value == null) {
+			if (other.value != null) {
+				return false;
+			}
+		} else if (!value.equals(other.value)) {
+			return false;
+		}
+		return true;
+	}
 }
