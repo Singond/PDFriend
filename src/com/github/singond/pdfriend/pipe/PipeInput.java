e71739d0a7eeaba9dd870a7602732163dc345459
Jan Singon Slany <slanyj@centrum.cz>
2017-11-16 00:32:00 +0100

    Declare PipeInput as closeable

--- src/com/github/singond/pdfriend/pipe/PipeInput.java
+++ src/com/github/singond/pdfriend/pipe/PipeInput.java
@@ -1,5 +1,7 @@
 package com.github.singond.pdfriend.pipe;
 
+import java.io.IOException;
+
 interface PipeInput extends AutoCloseable {
 
 	/**
@@ -10,4 +12,6 @@ interface PipeInput extends AutoCloseable {
 	
 	/** Checks whether there is unconsumed input. */
 	boolean hasMore();
+	
+	public void close() throws IOException;
 }
