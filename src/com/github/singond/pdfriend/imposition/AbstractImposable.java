adf49a0de1cc44eda4dd875bcb4ab5bef21e92ad
Jan Singon Slany <slanyj@centrum.cz>
2018-02-03 11:50:37 +0100

    Simplify Imposable interface

--- src/com/github/singond/pdfriend/imposition/AbstractImposable.java
+++ src/com/github/singond/pdfriend/imposition/AbstractImposable.java
@@ -6,15 +6,15 @@ import com.github.singond.pdfriend.document.VirtualDocument;
 
 public abstract class AbstractImposable implements Imposable {
 
-	@Override
-	public VirtualDocument imposeAndRender(VirtualDocument source) {
-		return impose(source).renderDocument();
-	}
-
-	@Override
-	public VirtualDocument imposeAndRender(List<VirtualDocument> sources) {
-		return impose(sources).renderDocument();
-	}
+//	@Override
+//	public VirtualDocument imposeAndRender(VirtualDocument source) {
+//		return impose(source).renderDocument();
+//	}
+//
+//	@Override
+//	public VirtualDocument imposeAndRender(List<VirtualDocument> sources) {
+//		return impose(sources).renderDocument();
+//	}
 	
 	/**
 	 * Returns a new builder of {@code PageSource} objects initialized to the
