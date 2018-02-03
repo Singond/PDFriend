4c0d254c91906f01bde8d3b8c81eaeb85e3674a1
Jan Singon Slany <slanyj@centrum.cz>
2018-02-03 12:30:37 +0100

    Make abstract implementation generic

--- src/com/github/singond/pdfriend/imposition/AbstractImposable.java
+++ src/com/github/singond/pdfriend/imposition/AbstractImposable.java
@@ -2,9 +2,10 @@ package com.github.singond.pdfriend.imposition;
 
 import java.util.List;
 
+import com.github.singond.pdfriend.book.Book;
 import com.github.singond.pdfriend.document.VirtualDocument;
 
-public abstract class AbstractImposable implements Imposable {
+abstract class AbstractImposable<T extends Book> implements Imposable<T> {
 
 //	@Override
 //	public VirtualDocument imposeAndRender(VirtualDocument source) {
