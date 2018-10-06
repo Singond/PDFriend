7a6f33d25ec94032cdfb06661061133c375b71d2
Jan Singon Slany <slanyj@centrum.cz>
2018-10-06 13:54:41 +0200

    Use data stream in Output

--- src/main/java/com/github/singond/pdfriend/format/RenderingManager.java
+++ src/main/java/com/github/singond/pdfriend/format/RenderingManager.java
@@ -9,23 +9,25 @@ import com.github.singond.pdfriend.io.OutputException;
 
 /**
  * Renders virtual documents into file format.
+ *
  * @author Singon
  */
 public class RenderingManager {
-	
+
 	/**
 	 * Renders multiple documents as a list of virtual documents.
-	 * @param output
-	 * @return
-	 * @throws OutputException
-	 * @throws RenderingException
+	 *
+	 * @param docs the documents to be rendered
+	 * @param output the output to write the rendered document into
+	 * @throws OutputException if an error occurs when opening the output
+	 * @throws RenderingException if an error occurs during rendering
 	 */
 	public final void renderDocuments(List<VirtualDocument> docs, Output output)
 			throws OutputException, RenderingException {
 		// TODO Handle different file formats?
 		PDFRenderer renderer = new PDFRenderer();
 		for (VirtualDocument doc : docs) {
-			output.acceptBytes(renderer.renderBinary(doc));
+			renderer.render(doc, output.getOutputStream());
 		}
 	}
 }
--- src/main/java/com/github/singond/pdfriend/io/Output.java
+++ src/main/java/com/github/singond/pdfriend/io/Output.java
@@ -1,11 +1,18 @@
 package com.github.singond.pdfriend.io;
 
+import java.io.OutputStream;
+
 /**
  * Represents data output.
  * @author Singon
  */
 public interface Output {
-	
-	/** Outputs the given byte array */
-	public void acceptBytes(byte[] bytes) throws OutputException;
+
+	/**
+	 * Returns a data stream ready to accept the data.
+	 *
+	 * @return an output stream
+	 * @throws OutputException if an error occurs opening the stream;
+	 */
+	public OutputStream getOutputStream() throws OutputException;
 }
