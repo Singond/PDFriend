9e89973544e20fbc8b369473b206a398fd71b13f
Jan Singon Slany <slanyj@centrum.cz>
2018-10-28 14:27:31 +0100

    Test cases without solution

--- src/test/java/com/github/singond/pdfriend/reorder/CoinChangeProblem.java
+++ src/test/java/com/github/singond/pdfriend/reorder/CoinChangeProblem.java
@@ -17,18 +17,30 @@ public class CoinChangeProblem {
 		combiner = new Combiner();
 	}
 
-	private void combine(int sum, Integer... values) {
+	private void solvable(int sum, Integer... values) {
 		List<Integer> s = combiner.combineToSum(sum, Arrays.asList(values));
 		int foundsum = s.stream().mapToInt(Integer::intValue).sum();
 		assertEquals("Bad sum of solution returned", sum, foundsum);
 		System.out.format("Picking from %s to a total of %d: %s%n",
-		                  Arrays.asList(values), sum, s);
+				Arrays.asList(values), sum, s);
+	}
+
+	private void unsolvable(int sum, Integer... values) {
+		List<Integer> s = combiner.combineToSum(sum, Arrays.asList(values));
+		assertEquals("Found solution where none exists", null, s);
+		System.out.format("Picking from %s to a total of %d: no solution%n",
+		                  Arrays.asList(values), sum);
 	}
 
 	@Test
 	public void findingSolution() {
-		combine(5, 1, 2, 3);
-		combine(13, 1, 1, 2, 3, 6, 8);
-		combine(37, 1, 1, 2, 3, 6, 8, 4, 4, 9, 7, 16, 10);
+		solvable(5, 1, 2, 3);
+		solvable(13, 1, 1, 2, 3, 6, 8);
+		solvable(37, 1, 1, 2, 3, 6, 8, 4, 4, 9, 7, 16, 10);
+	}
+
+	@Test
+	public void findingNoSolution() {
+		unsolvable(6, 1, 3);
 	}
 }
