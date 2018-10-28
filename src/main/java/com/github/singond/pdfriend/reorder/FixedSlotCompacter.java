ee49c6f2fa28dd42dda941771ee2784c1f610f2a
Jan Singon Slany <slanyj@centrum.cz>
2018-10-28 19:51:00 +0100

    Implement compacting into fixed slots

--- src/main/java/com/github/singond/pdfriend/reorder/FixedSlotCompacter.java
+++ src/main/java/com/github/singond/pdfriend/reorder/FixedSlotCompacter.java
@@ -18,19 +18,50 @@ class FixedSlotCompacter<T> implements Compacter<T> {
 	/**
 	 * A buffer of unplaced objects, sorted by their size.
 	 */
-	private final NumberedQueue<List<Element>> unplaced;
+	private final NumberedQueue<Element> unplaced;
 
 	private int elementCounter = 0;
+
+	private Combiner combiner;
 
 	FixedSlotCompacter(int slotSize) {
 		this.slotSize = slotSize;
 		placed = new ArrayList<>();
 		unplaced = new NumberedQueue<>();
+		combiner = new Combiner();
 	}
 
 	@Override
 	public void add(T element, int size) {
+		if (size < 0) {
+			throw new IllegalArgumentException("Size must not be negative");
+		}
+		if (element == null) {
+			throw new NullPointerException("The element to be added is null");
+		}
 
+		// If the size is an integer number of slots, place it now,
+		// else store it away for later placement
+		if (size % slotSize == 0) {
+			placed.add(element);
+		} else {
+			unplaced.add(new Element(element), size);
+		}
+	}
+
+	/**
+	 * Attempts to place the unplaced objects now into n slots.
+	 * If any combination of unplaced objects can be made that has size
+	 * equal to the size of n slots, place it now.
+	 */
+	private void tryFlush(int slots) {
+		List<Integer> sizes = unplaced.numbers();
+		List<Integer> sizeComb = combiner.combineToSum(slots * slotSize, sizes);
+		if (sizeComb != null) {
+			for (int length : sizeComb) {
+				placed.add(unplaced.nextInQueue(length).value());
+			}
+		}
 	}
 
 	/**
@@ -44,6 +75,10 @@ class FixedSlotCompacter<T> implements Compacter<T> {
 		public Element(T value) {
 			this.value = value;
 			this.order = elementCounter++;
+		}
+
+		public T value() {
+			return value;
 		}
 
 		@Override
