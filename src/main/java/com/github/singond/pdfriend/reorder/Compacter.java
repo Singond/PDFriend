28cb1d2c9ab2d03864e068253983fdd4bbff4f21
Jan Singon Slany <slanyj@centrum.cz>
2018-10-30 22:11:40 +0100

    Edit Compacter interface doc

--- src/main/java/com/github/singond/pdfriend/reorder/Compacter.java
+++ src/main/java/com/github/singond/pdfriend/reorder/Compacter.java
@@ -4,14 +4,15 @@ import java.util.Collection;
 import java.util.List;
 
 /**
- * A compacter for arbitrary objects. This takes a sequence of 'slots'
+ * A compacter for arbitrary objects. This takes a sequence of 'sections'
  * with integer length and a number of objects with specified 'size',
  * which is an integer number indicating how many units they take up
- * in the slot length.
+ * in the section.
  *
  * It then attempts to order the objects so that when placed one after another
- * into the slots, the number of cases where an object overlaps slot boundary
- * is minimized. It is generally not required to find the optimal solution.
+ * into the sections, the number of cases where an object overlaps section
+ * boundary is minimized.
+ * It is generally not required to find the optimal solution.
  *
  * @author Singon
  * @param <T> the type of the objects being compacted
@@ -23,7 +24,6 @@ interface Compacter<T> {
 	 * as described in the interface description.
 	 *
 	 * @param objects the objects to be ordered
-	 * @param size a function providing the 'size' of elements in {@code objects}
 	 */
 	public List<T> process(Collection<T> objects);
 }
