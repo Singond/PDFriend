58aa1309bc587c09182a52a78320fd07626f0573
Jan Singon Slany <slanyj@centrum.cz>
2018-11-01 22:43:34 +0100

    Support any collection in formatter

--- src/main/java/com/github/singond/pdfriend/util/Formatting.java
+++ src/main/java/com/github/singond/pdfriend/util/Formatting.java
@@ -1,6 +1,6 @@
 package com.github.singond.pdfriend.util;
 
-import java.util.List;
+import java.util.Collection;
 
 public class Formatting {
 
@@ -9,11 +9,11 @@ public class Formatting {
 	}
 
 	/**
-	 * Produces a short description of the contents of a given list.
-	 * For short lists (whose length does not exceed the given limit),
-	 * the output is the list's own {@code toString} method.
-	 * For longer lists, elements after the limit are truncated and their
-	 * count is appended.
+	 * Produces a short description of the contents of a given collection.
+	 * For small collections (whose size does not exceed the given limit),
+	 * the output is the collection's own {@code toString} method.
+	 * For longer collections, elements after the limit are truncated and
+	 * their count is appended.
 	 * <p>
 	 * Given a list of the capital letters of the English alphabet (A-Z),
 	 * the output will look similar to this (the limit is set to {@code 4}):
@@ -21,19 +21,26 @@ public class Formatting {
 	 * [A, B, C, D... (22 more)]
 	 * </pre>
 	 *
-	 * @param list the list whose contents are to be formatted
+	 * @param <T> the type of elements in the collection
+	 * @param coll the collection whose contents are to be formatted
 	 * @param limit the maximum number of elements to show
 	 * @return a string builder representing the first {@code limit} elements
-	 *         of {@code list} and the number of remaining elements
+	 *         of {@code coll} and the number of remaining elements
 	 */
-	public static StringBuilder listDigest(List<?> list, int limit) {
+	public static <T> StringBuilder listDigest(Collection<T> coll, int limit) {
 		StringBuilder sb = new StringBuilder();
-		if (list.size() <= limit) {
-			sb.append(list.toString());
+		if (coll.size() <= limit) {
+			sb.append(coll.toString());
 		} else {
-			sb.append(list.subList(0, limit).toString());
-			sb.setLength(sb.length() - 1);
-			sb.append("... (" + (list.size() - limit) + " more)]");
+			sb.append("[");
+			int index = 0;
+			for (T element : coll) {
+				if (index++ < limit) {
+					sb.append(element).append(", ");
+				}
+			}
+			sb.setLength(sb.length() - 2);
+			sb.append("... (" + (coll.size() - limit) + " more)]");
 		}
 		return sb;
 	}
